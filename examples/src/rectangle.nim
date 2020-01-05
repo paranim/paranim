ca76f06158dcd2f3ee7d5b1dc42e6cfa2fc9a1a1
oakes <zach@sekao.net>
2020-01-05 15:25:38 -0500

    Move utils into gl submodule

--- examples/src/rectangle.nim
+++ examples/src/rectangle.nim
@@ -1,6 +1,6 @@
 import nimgl/[glfw, opengl]
 import glm
-import parabola/utils, parabola/math
+import parabola/gl/utils, parabola/math
 
 proc keyProc(window: GLFWWindow, key: int32, scancode: int32,
              action: int32, mods: int32): void {.cdecl.} =
