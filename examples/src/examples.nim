ce188fe15af664ce2ed4a835d8bbf45b21a66065
oakes <zach@sekao.net>
2020-02-07 10:49:28 -0500

    Show example name in title bar

--- examples/src/examples.nim
+++ examples/src/examples.nim
@@ -17,27 +17,27 @@ from ex14_perspective_animation_3d import nil
 from ex15_perspective_texture_3d import nil
 
 const examples = [
-  (init: ex01_image.init, tick: ex01_image.tick),
-  (init: ex02_rect.init, tick: ex02_rect.tick),
-  (init: ex03_rand_rects.init, tick: ex03_rand_rects.tick),
-  (init: ex04_translation.init, tick: ex04_translation.tick),
-  (init: ex05_rotation.init, tick: ex05_rotation.tick),
-  (init: ex06_scaling.init, tick: ex06_scaling.tick),
-  (init: ex07_rotation_multiple.init, tick: ex07_rotation_multiple.tick),
-  (init: ex08_translation_3d.init, tick: ex08_translation_3d.tick),
-  (init: ex09_rotation_3d.init, tick: ex09_rotation_3d.tick),
-  (init: ex10_scaling_3d.init, tick: ex10_scaling_3d.tick),
-  (init: ex11_perspective_3d.init, tick: ex11_perspective_3d.tick),
-  (init: ex12_perspective_camera_3d.init, tick: ex12_perspective_camera_3d.tick),
-  (init: ex13_perspective_camera_target_3d.init, tick: ex13_perspective_camera_target_3d.tick),
-  (init: ex14_perspective_animation_3d.init, tick: ex14_perspective_animation_3d.tick),
-  (init: ex15_perspective_texture_3d.init, tick: ex15_perspective_texture_3d.tick),
+  (init: ex01_image.init, tick: ex01_image.tick, name: "ex01_image"),
+  (init: ex02_rect.init, tick: ex02_rect.tick, name: "ex02_rect"),
+  (init: ex03_rand_rects.init, tick: ex03_rand_rects.tick, name: "ex03_rand_rects"),
+  (init: ex04_translation.init, tick: ex04_translation.tick, name: "ex04_translation"),
+  (init: ex05_rotation.init, tick: ex05_rotation.tick, name: "ex05_rotation"),
+  (init: ex06_scaling.init, tick: ex06_scaling.tick, name: "ex06_scaling"),
+  (init: ex07_rotation_multiple.init, tick: ex07_rotation_multiple.tick, name: "ex07_rotation_multiple"),
+  (init: ex08_translation_3d.init, tick: ex08_translation_3d.tick, name: "ex08_translation_3d"),
+  (init: ex09_rotation_3d.init, tick: ex09_rotation_3d.tick, name: "ex09_rotation_3d"),
+  (init: ex10_scaling_3d.init, tick: ex10_scaling_3d.tick, name: "ex10_scaling_3d"),
+  (init: ex11_perspective_3d.init, tick: ex11_perspective_3d.tick, name: "ex11_perspective_3d"),
+  (init: ex12_perspective_camera_3d.init, tick: ex12_perspective_camera_3d.tick, name: "ex12_perspective_camera_3d"),
+  (init: ex13_perspective_camera_target_3d.init, tick: ex13_perspective_camera_target_3d.tick, name: "ex13_perspective_camera_target_3d"),
+  (init: ex14_perspective_animation_3d.init, tick: ex14_perspective_animation_3d.tick, name: "ex14_perspective_animation_3d"),
+  (init: ex15_perspective_texture_3d.init, tick: ex15_perspective_texture_3d.tick, name: "ex15_perspective_texture_3d"),
 ]
 
 var game = Game()
 var currentExample = 0
 
-proc updateExample(direction: int) =
+proc updateExample(window: GLFWWindow, direction: int) =
   var newExample = currentExample + direction
   if newExample < 0:
     newExample = examples.len - 1
@@ -45,6 +45,7 @@ proc updateExample(direction: int) =
     newExample = 0
   examples[newExample].init(game)
   currentExample = newExample
+  window.setWindowTitle(examples[currentExample].name)
 
 proc keyCallback(window: GLFWWindow, key: int32, scancode: int32,
                  action: int32, mods: int32): void {.cdecl.} =
@@ -52,9 +53,9 @@ proc keyCallback(window: GLFWWindow, key: int32, scancode: int32,
     if key == GLFWKey.Escape:
       window.setWindowShouldClose(true)
     elif key == GLFWKey.Left:
-      updateExample(-1)
+      updateExample(window, -1)
     elif key == GLFWKey.Right:
-      updateExample(1)
+      updateExample(window, 1)
 
 proc mousePositionCallback(window: GLFWWindow, xpos: float64, ypos: float64): void {.cdecl.} =
   game.mouseX = xpos
